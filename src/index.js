fa43e4af654dd94ff12c7e7826defec4332ed81e
jsant0s3189 <jsant0s3189@gmail.com>
2018-01-27 14:28:55 -0600

    feat: add random photo

--- src/index.js
+++ src/index.js
@@ -5,15 +5,69 @@
  * @license The MIT License (MIT)
  */
 
-(function($){
+(function(global, $){
+  //funciones de sentencia
+  function BgRandom(){
 
-  $.fn.bgRandom = function() {
-    $(this).css({
-      width: '100%',
-      heigth: '100vh',
-      minHeight: '800px'
-    });
-    return $(this);
+  }
+
+  //funcion declarativa
+  BgRandom.prototype.setup = function(clientId) {
+    this.clientId = clientId;
   };
 
-})(jQuery);
+  var getPhoto = function(){
+    return (
+      $.ajax({
+        url: 'https://api.unsplash.com/photos/random',
+        beforeSend: function(request) {
+          request.setRequestHeader('Authorization', 'Client-ID ' + global.BgRandom.clientId);
+        }
+      })
+    );
+  };
+
+  $.fn.bgRandom = function(options) {
+    options = options || {};
+    var $self = $(this);
+    $self.css({
+      width: '100%',
+      heigth: '100vh',
+      minHeight: options.minHeight || '800px',
+      backgroundSize: options.backgroundSize || 'cover',
+      backgroundPosition: options.backgroundPosition || 'center',
+      backgroundColor: options.backgroundColor || 'black',
+    });
+
+    if (options.usePromise) {
+      var def = $.Deferred();
+      getPhoto().then(function(photo){
+        $self.css({
+          backgroundImage: 'url(' + photo.urls.regular + ')'
+        });
+        def.resolve($self);
+      })
+        .catch(function(){
+          $self.css({
+            backgroundImage: 'url(' + options.backgroundImage + ')'
+          });
+          def.reject($self);
+        });
+      return def.promise();
+    }
+
+    getPhoto().then(function(photo){
+      $self.css({
+        backgroundImage: 'url(' + photo.urls.regular + ')'
+      });
+    })
+      .catch(function(){
+        $self.css({
+          backgroundImage: 'url(' + options.backgroundImage + ')'
+        });
+      });
+    return $self;
+  };
+
+  global.BgRandom = new BgRandom();
+})(window, jQuery);
